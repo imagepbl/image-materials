bded6214eb322ce7d18c2bf320f24b319c71ee3a
Raoul Schram <r.d.schram@uu.nl>
2024-08-16 14:19:22 +0200

    Restructure survival function (during meeting)

--- prism-stock-classes/Stock.py
+++ prism-stock-classes/Stock.py
@@ -4,16 +4,57 @@ import numpy as np
 import scipy
 
 
-class Survival:
+# surv[t_idx, cohort_idx]
+
+# TODO: Move survival functions and classes to separate module.
+# TODO: Switch to XArray
+
+class DynamicSurvival:
+    def __init__(self, compute_next, num_timesteps):
+        # TODO: Add docstrings
+        self.compute_next = compute_next
+        self.survival_matrix = np.zeros((num_timesteps, num_timesteps))
+        self.num_timesteps
+        self._cached_timesteps = set()
+
+    def __getitem__(self, idx):
+        t_idx, cohort_idx = idx
+        # Is the column already computed?
+        if cohort_idx not in self._cached_timesteps:
+            # TODO: Good explanation here..
+            self.survival_matrix[cohort_idx:, cohort_idx] = self.compute_next(self.num_timestep, cohort_idx)
+            self._cached_timesteps.add(cohort_idx)
+        return self.survival_matrix[t_idx, cohort_idx]
+
+
+# TODO adapt to new structure of DynamicSurvival
+
+class StaticSurvival:
     def __init__(self, survival_matrix):
         self.survival_matrix = survival_matrix
 
-    def compute(t, cohort):
+    def compute(self, t, cohort):
         return self.survival_matrix[t, cohort]
 
 
+surv = DynamicSurvival(
+    compute_next=lambda num_timesteps, cohort_idx: weibull_survival(num_timesteps, cohort_idx, shape=1, scale=2),
+    num_timesteps=1000
+)
+
+def weibull_survival(num_timesteps, cohort_idx, shape, scale):
+    return scipy.stats.weibull_min.sf(
+                np.arange(num_timesteps-cohort_idx),
+                c=shape[cohort_idx],
+                loc=0,
+                scale=scale[cohort_idx],
+    )
+
+# TODO: add folded normal
+
+# Remove
 @prism.interface
-class WeibullSurvival(Survival):
+class WeibullSurvival(StaticSurvival):
     shape: float
     scale: float
 
@@ -21,15 +62,16 @@ class WeibullSurvival(Survival):
         # Calculate matrix here
         survival_matrix = np.empty((num_timesteps, num_timesteps))
         for t in range(num_timesteps):
-            survival_matrix[t:, :] = scipy.stats.weibull_min.sf(
+            survival_matrix[t:, t] = scipy.stats.weibull_min.sf(
                 np.arange(num_timesteps-t),
-                c=shape[t]
+                c=shape[t],
                 loc=0,
                 scale=scale[t],
             )
         super().__init__(survival_matrix)
 
 
+# To be removed
 @prism.interface
 class FoldedNormalSurvival(Survival):
     mean: float
@@ -37,10 +79,11 @@ class FoldedNormalSurvival(Survival):
 
     def __init__(self, mean, std, num_timesteps):
         # Calculate matrix here
+        survival_matrix = np.empty((num_timesteps, num_timesteps))
         for t in num_timesteps:
             survival_matrix[t:, :] = scipy.stats.foldnorm.sf(
                 np.arange(num_timesteps-t),
-                c=mean[t]/std[t]
+                c=mean[t]/std[t],
                 loc=0,
                 scale=std[t],
             )
@@ -49,16 +92,14 @@ class FoldedNormalSurvival(Survival):
 
 class Stock:
     def __init__():
-
+        pass
     
 
     def compute(
-    self,
-    time: prism.Time,
-    input_stock: prism.Array['count'],
-    survival: Survival
-    
-    ):
+            self,
+            time: prism.Time,
+            input_stock: prism.Array['count'],
+            survival: Survival):
         t = time.t
         dt = time.dt
 
@@ -94,9 +135,3 @@ class Survival:
             )
 
         return survival
-
-
-
-
-
-
