20d20d040276b7f47209936d31f3ffafc5120d5e
Raoul Schram <r.d.schram@uu.nl>
2024-08-28 13:29:13 +0200

    Fix survival class

--- prism-stock-classes/survival.py
+++ prism-stock-classes/survival.py
@@ -2,15 +2,18 @@ import numpy as np
 import scipy
 from itertools import islice
 import xarray as xr
+from abc import abstractmethod, ABC
 
-class Survival:
-    def __init__(self, time_series):
+class SurvivalMatrix:
+    def __init__(self, survival):
         # TODO: Add docstrings
-        # self.compute_next = compute_next
-        self.survival_matrix = xr.DataArray(0.0, dims=("time", "cohort"),
-                                            coords={"time": time_series, "cohort": time_series})
+        self.survival_matrix = xr.DataArray(0.0, dims=("time", "cohort", "mode"),
+                                            coords={"time": survival.time_series.to_numpy(),
+                                                    "cohort": survival.time_series.to_numpy(),
+                                                    "mode": survival.modes})
         self._cached_timesteps = set()
-        self.num_timesteps = len(time_series)
+        self.num_timesteps = len(survival.time_series)
+        self.survival = survival
 
     def __getitem__(self, idx):
         # TODO: make the computation dependent on t_idx
@@ -24,46 +27,60 @@ class Survival:
             comp_list = islice(self.survival_matrix.indexes["cohort"], *cohort.indices(self.num_timesteps))
         else:
             comp_list = [cohort]
-            
+
         # Is the column already computed?
         for cur_cohort in comp_list:
             if cur_cohort not in self._cached_timesteps:
                 # Compute the survival matrix for the current cohort_idx
                 # The survival coeficients are only relevant for future times t
                 # So the values are created in column cohort_idx, for t_idx=cohort_idx, cohort_idx+1, ...
-                self.survival_matrix.loc[cur_cohort:, cur_cohort] = self.compute_survival(cur_cohort)
+                for mode in self.survival.modes:
+                    self.survival_matrix.loc[cur_cohort:, cur_cohort, mode] = self.survival.compute_survival(mode, cur_cohort)
                 self._cached_timesteps.add(cur_cohort)
         return self.survival_matrix.loc[t, cohort]
 
-    def compute_survival(self, cohort):
-        cohort_idx = self.survival_matrix.indexes["cohort"].get_loc(cohort)
 
-        cur_param = {}
-        for par, par_val in self.param.items():
-            if _is_iterable(par_val):
-                cur_param[par] = par_val[cohort_idx]
-            else:
-                cur_param[par] = par_val
+class ScipySurvival(ABC):
+    def __init__(self, lifetime_parameters):
+        self.lifetime_parameters = lifetime_parameters
+
+    def compute_survival(self, mode, cohort):
+        n_coords_left = len(self.time_series.loc[cohort:])
         return self._method(
-            np.arange(len(self.survival_matrix.coords["cohort"]) - cohort_idx),
-            **cur_param
+            np.arange(n_coords_left),
+            **self.get_param(mode, cohort)
         )
 
+    @abstractmethod
+    def get_param(self, mode, cohort):
+        raise NotImplementedError()
 
-class WeibullSurvival(Survival):
+    @property
+    def modes(self):
+        return np.unique([x[0] for x in self.lifetime_parameters.data_vars])
+
+    @property
+    def time_series(self):
+        return self.lifetime_parameters.coords["year"]
+
+class WeibullSurvival(ScipySurvival):
     _method = scipy.stats.weibull_min.sf
 
-    def __init__(self, time_series, shape, scale):
-        self.param = {"c": shape, "scale": scale, "loc": 0}
-        super().__init__(time_series)
+    def get_param(self, mode, cohort):
+        return {
+            "c": self.lifetime_parameters[mode, "shape"].loc[cohort],
+            "scale": self.lifetime_parameters[mode, "scale"].loc[cohort],
+            "loc": 0
+        }
 
-class FoldedNormalSurvival(Survival):
+
+class FoldedNormalSurvival(ScipySurvival):
     _method = scipy.stats.foldnorm.sf
 
-    def __init__(self, time_series, mean, stdev):
-        self.param = {"c": mean/stdev, "loc": 0, "scale": stdev}
-        super().__init__(time_series)
-
+    def get_param(self, mode, cohort):
+        mean = self.lifetime_parameters[mode, "mean"].loc[cohort]
+        stdev = self.lifetime_parameters[mode, "stdev"].loc[cohort]
+        return {"c": mean/stdev, "scale": stdev, "loc": 0}
 
 def _is_iterable(val):
     try:
