ea8ac338f617011fe5c0758b4350fa939d07fe05
Roel Brouwer <5979700+RoelBrouwer@users.noreply.github.com>
2024-10-17 11:21:13 +0200

    Remove redundant if-statement

--- imagematerials/vehicles/__main__.py
+++ imagematerials/vehicles/__main__.py
@@ -4,8 +4,7 @@ from vemamodeling import run_model
 from visualization import visualize_data
 
 
-if __name__ == "__main":
-    prep_data = preprocess()
-    raw_data = run_model(prep_data)
-    processed_data = postprocess(raw_data)
-    visualize_data(processed_data)
+prep_data = preprocess()
+raw_data = run_model(prep_data)
+processed_data = postprocess(raw_data)
+visualize_data(processed_data)
