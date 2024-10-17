7137f38294e7432ee6868e8467d2a1026741d35d
Roel Brouwer <5979700+RoelBrouwer@users.noreply.github.com>
2024-10-17 11:21:35 +0200

    Fix preprocessing import

--- imagematerials/vehicles/__main__.py
+++ imagematerials/vehicles/__main__.py
@@ -1,10 +1,10 @@
-from preprocessing import preprocess
+from preprocessing import preprocessing
 from postprocessing import postprocess
 from vemamodeling import run_model
 from visualization import visualize_data
 
 
-prep_data = preprocess()
+prep_data = preprocessing()
 raw_data = run_model(prep_data)
 processed_data = postprocess(raw_data)
 visualize_data(processed_data)
