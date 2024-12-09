d78db6b4d3569124282dfd363caa04f8514510e1
Arp <a.f.arp@uu.nl>
2024-12-09 16:18:46 +0100

    fixed 10% bug and changed base path to fit new folder structure

--- imagematerials/_version.py
+++ imagematerials/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev185+g57c16be.d20240925'
-__version_tuple__ = version_tuple = (0, 1, 'dev185', 'g57c16be.d20240925')
+__version__ = version = '0.1.dev219+g1eff4fe.d20241209'
+__version_tuple__ = version_tuple = (0, 1, 'dev219', 'g1eff4fe.d20241209')
