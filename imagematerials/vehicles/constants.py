7dce87e926abf5c5ba961280b82d2bc4b119696d
Luja von Köckritz <l.m.s.vonkockritz@uu.nl>
2025-01-10 09:58:52 +0100

    change data structure

--- imagematerials/vehicles/constants.py
+++ imagematerials/vehicles/constants.py
@@ -154,9 +154,9 @@ vshares_label    = ['ICE', 'HEV', 'PHEV', 'BEV', 'FCV', 'Trolley']
 # --- Paths
 
 # Scenario settings
-SCEN = "SSP2_M_"
+SCEN = "SSP2_M"
 # CP or 2D (Add "_RE" for Resource Efficiency)
-VARIANT = "CP"
+VARIANT = "_CP"
 PROJECT = "mock_project"
 FOLDER = SCEN + "_" + VARIANT
 OUTPUT_FOLDER = base_dir.joinpath("..", "..", "output", PROJECT, FOLDER)
