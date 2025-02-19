595344f0a1de3e8eba5f71f185b027a9741fb00a
Luja von Köckritz <l.m.s.vonkockritz@uu.nl>
2025-02-19 15:15:09 +0100

    adap to run generic model

--- imagematerials/model.py
+++ imagematerials/model.py
@@ -124,7 +124,7 @@ class Maintenance(prism.Model):
                     "Type": self.Type,
                     "material": self.material})
 
-    def compute_values(self, time: prism.Time, inflow, stock_by_cohort, outflow_by_cohort):
+    def compute_values(self, time: prism.Time, stock_by_cohort):
         t, dt = time.t, time.dt
         self.stock_by_cohort_maintenance_materials.loc[t] = (stock_by_cohort.loc[t]*self.maintenance_material_fractions*self.weights).sum("Cohort")
 
@@ -203,5 +203,5 @@ class GenericMainModel(prism.Model):
                                                stock_by_cohort=self.stock_model.stock_by_cohort,
                                                outflow_by_cohort=self.stock_model.outflow_by_cohort)
         if self.compute_maintenance_materials:
-            self.maintenance_model.compute_values(time, )
+            self.maintenance_model.compute_values(time, stock_by_cohort=self.stock_model.stock_by_cohort)
 
