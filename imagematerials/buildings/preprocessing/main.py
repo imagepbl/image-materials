a8d16e6c558c6d87c84194cf91a96ba79cc4b3a8
Arp <a.f.arp@uu.nl>
2025-05-23 14:34:43 +0200

    replaced total population csv to scn image data and repaired according functions

--- imagematerials/buildings/preprocessing/main.py
+++ imagematerials/buildings/preprocessing/main.py
@@ -41,7 +41,7 @@ def buildings_preprocessing(base_directory):
     floorspace_commercial = floorspace_commercial_rururb.sel(
         {"Type": [x.values for x in floorspace_commercial_rururb.coords["Type"] if x.values not in ["Urban", "Rural"]]})
 
-    population = compute_population(base_directory)
+    population = compute_population(image_directory, base_directory)
     average_m2_capita = compute_average_m2_capita(base_directory)
 
     housing_type = compute_housing_type(database_directory)
