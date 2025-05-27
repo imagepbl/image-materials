df058c154b3122bfe8bf17142c0ff565774495fc
MarianneZZotin <m.zanonzotin@uu.nl>
2025-05-27 10:18:33 +0200

    update in eol testing

--- imagematerials/end_of_life/preprocessing/main.py
+++ imagematerials/end_of_life/preprocessing/main.py
@@ -10,6 +10,6 @@ def eol_preprocessing (xr_collection, xr_reuse, xr_recycling)
     # inter and extrapolated collection, reuse and recycling xr 
     collection = compute_eol(xr_collection, start_year= start_year, end_year=end_year,min_value = 0,max_value = 1)
     reuse = compute_eol(xr_reuse, start_year= start_year, end_year=end_year,min_value = 0,max_value = 1)
-    recyling = compute_eol(xr_recycling, start_year= start_year, end_year=end_year,min_value = 0,max_value = 1)
+    recycling = compute_eol(xr_recycling, start_year= start_year, end_year=end_year,min_value = 0,max_value = 1)
 
     return collection, reuse, recycling
