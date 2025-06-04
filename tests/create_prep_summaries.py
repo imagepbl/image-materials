e15231d722edba5fc55015843e1876f46f3504f4
Luja von Köckritz <l.m.s.vonkockritz@uu.nl>
2025-06-04 17:02:45 +0200

    update test

--- tests/create_prep_summaries.py
+++ tests/create_prep_summaries.py
@@ -25,12 +25,15 @@ if __name__ == "__main__":
     new_prep_data["knowledge_graph"] = knowledge_graph
     new_prep_data["weights"] = new_prep_data.pop("vehicle_weights")
 
-    summary_vhc = summarize_prep_data(new_prep_data, climate_policy_config, circular_economy_config)
+    summary_vhc = summarize_prep_data(new_prep_data)
     with open(Path("tests", "data", "vehicles_summary.json"), "w", encoding="utf8") as handle:
         json.dump(summary_vhc, handle)
 
     # Buildings summary
-    bld_sector = get_preprocessing_data("buildings", Path("data", "raw"))
-    summary_bld = summarize_prep_data(bld_sector.prep_data)
+    prep_data_bld = bld.preprocess(Path("data", "raw"), climate_policy_config, circular_economy_config)
+    new_prep_data = {k: v for k, v in prep_data_bld.items()}
+    new_prep_data["knowledge_graph"] = create_building_graph()
+
+    summary_bld = summarize_prep_data(new_prep_data)
     with open(Path("tests", "data", "buildings_summary.json"), "w", encoding="utf8") as handle:
         json.dump(summary_bld, handle)
