81d7680a145803ae3a95bc6cd3315073237f1145
Luja von Köckritz <l.m.s.vonkockritz@uu.nl>
2025-06-04 17:07:06 +0200

    update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,7 +32,11 @@ def vhc_prep_data():
 
 @pytest.fixture(scope="session")
 def bld_prep_data():
-    prep_data = bld.preprocess(Path("data", "raw"))
+    climate_policy_scenario_dir = Path("data", "raw") / 'SSP2'
+    circular_economy_scenario_dirs = {"slow": Path("data", "raw") / 'circular_economy_scenarios' / 'slow'}
+    climate_policy_config = read_climate_policy_config(climate_policy_scenario_dir)
+    circular_economy_config = read_circular_economy_config(circular_economy_scenario_dirs)
+    prep_data = bld.preprocess(Path("data", "raw"), climate_policy_config, circular_economy_config)
     new_prep_data = {k: v for k, v in prep_data.items()}
     new_prep_data["knowledge_graph"] = create_building_graph()
     new_prep_data["shares"] = None
