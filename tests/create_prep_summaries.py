5db6ca650857544b012d5f6a465e5c17c0ead555
Luja von Köckritz <l.m.s.vonkockritz@uu.nl>
2025-06-04 17:01:58 +0200

    update test

--- tests/create_prep_summaries.py
+++ tests/create_prep_summaries.py
@@ -10,8 +10,22 @@ from imagematerials.util import (
 
 if __name__ == "__main__":
     # Vehicles summary
-    vhc_sector = get_preprocessing_data("vehicles", Path("data", "raw"))
-    summary_vhc = summarize_prep_data(vhc_sector.prep_data)
+    climate_policy_scenario_dir = Path("data", "raw") / 'SSP2'
+    circular_economy_scenario_dirs = {"slow": Path("data", "raw") / 'circular_economy_scenarios' / 'slow'}
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        climate_policy_config = read_climate_policy_config(climate_policy_scenario_dir)
+        circular_economy_config = read_circular_economy_config(circular_economy_scenario_dirs)
+
+        prep_data = vhc.preprocess(Path("data", "raw"), climate_policy_config, circular_economy_config)
+    knowledge_graph = create_vehicle_graph()
+    new_prep_data = rebroadcast_prep_data(prep_data, knowledge_graph, dim="Type", output_coords=prep_data["shares"].coords["Type"].values)
+    new_prep_data = rebroadcast_prep_data(new_prep_data, knowledge_graph, dim="Region", output_coords=prep_data["shares"].coords["Region"].values)
+    new_prep_data["knowledge_graph"] = knowledge_graph
+    new_prep_data["weights"] = new_prep_data.pop("vehicle_weights")
+
+    summary_vhc = summarize_prep_data(new_prep_data, climate_policy_config, circular_economy_config)
     with open(Path("tests", "data", "vehicles_summary.json"), "w", encoding="utf8") as handle:
         json.dump(summary_vhc, handle)
 
