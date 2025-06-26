7e432812098c470a09ecfbf07e67fd8812600498
MarianneZZotin <m.zanonzotin@uu.nl>
2025-06-26 08:16:02 +0200

    change data input for eol to CP

--- imagematerials/eol/constants.py
+++ imagematerials/eol/constants.py
@@ -12,7 +12,7 @@ full_time = np.arange(start_year, end_year + 1)
 
 # scenario
 base_scenario    = "SSP2"
-scenario_variant = "2D_RE"    # CP = Current Policies, 2D = 2-Degree Climate Policy, RE indicates additional resource efficiency assumptions
+scenario_variant = "CP"    # CP = Current Policies, 2D = 2-Degree Climate Policy, RE indicates additional resource efficiency assumptions
 SCENARIO_SELECT = base_scenario + "_" + scenario_variant
 
 # End-of-Life types
