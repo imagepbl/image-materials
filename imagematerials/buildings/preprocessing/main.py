5b011695317d51a723acd7bf0a8ff10b22b05de6
Luja von Köckritz <l.m.s.vonkockritz@uu.nl>
2025-06-30 16:34:08 +0200

    Update climate scenario path in buildings

--- imagematerials/buildings/preprocessing/main.py
+++ imagematerials/buildings/preprocessing/main.py
@@ -24,7 +24,8 @@ from imagematerials.concepts import create_building_graph
 def buildings_preprocessing(base_directory, climate_policy_config: dict, circular_economy_config: dict):
     base_directory = Path(base_directory)
     database_directory = base_directory / "buildings" / SCENARIO_SELECT
-    image_directory = base_directory / "IMAGE_CircoMod" / "SSP2"
+    image_directory = climate_policy_config["config_file_path"]
+    print(image_directory)
     assert database_directory.is_dir(), database_directory
     assert image_directory.is_dir(), image_directory
 
