f8d13991ad0b13a02a063184212b2789b836a120
Raoul Schram <r.d.schram@uu.nl>
2025-07-09 12:31:57 +0200

    Add save_pkl to saved model

--- imagematerials/factory.py
+++ imagematerials/factory.py
@@ -1,4 +1,6 @@
 """Module to dynamically create models."""
+import pickle as pkl
+from pathlib import Path
 from typing import Any, Optional, Union
 
 import prism
@@ -237,10 +239,23 @@ class ModelFactory():
                     return super().__getattribute__(attr)
                 except AttributeError:
                     if len(factory.sectors) == 1:
-                        return list(factory.sectors.values())[0].all_data[attr]
-                    return {sec_name: factory.sectors[sec_name].all_data[attr]
-                            for sec_name in factory.sectors
-                            if attr in factory.sectors[sec_name].all_data}
+                        try:
+                            return list(factory.sectors.values())[0].all_data[attr]
+                        except KeyError as exc:
+                            raise AttributeError(f"Cannot find attribute {attr} for "
+                                                 f"{self.__class__}") from exc
+                    all_data = {sec_name: factory.sectors[sec_name].all_data[attr]
+                                for sec_name in factory.sectors
+                                if attr in factory.sectors[sec_name].all_data}
+                    if len(all_data) == 0:
+                        raise AttributeError(f"Cannot find attribute {attr} for "
+                                             f"{self.__class__}")
+                    return all_data
+
+            def save_pkl(self, data_fp: Union[Path, str]):
+                all_data = {sec_name: getattr(self, sec_name) for sec_name in factory.sectors}
+                with open(data_fp, "wb") as handle:
+                    pkl.dump(all_data, handle)
 
         main_model = MainModule(self.complete_timeline)
 
