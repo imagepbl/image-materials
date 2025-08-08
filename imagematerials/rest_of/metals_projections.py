18e0ecacd310359a9e21f70b8b11a1c7b21b5403
Frederike Arp <152859146+frede-arp@users.noreply.github.com>
2025-08-08 09:05:37 +0200

    301 rest of fossil fuels and visualization (#304)
    * Rest of scenarios
    
    * rest of scenarios with image materials start
    
    * save

--- imagematerials/rest_of/metals_projections.py
+++ imagematerials/rest_of/metals_projections.py
@@ -66,7 +66,7 @@ def copper_projection(scenario: str):
         
     copper.smooth_out_interpolation_all(10, 2017)
     copper.adjust_alpha_and_project(list(COPPER_AVERAGE_REGIONS_TO_IMAGE.keys()), 
-                            start_year_adjust=2050, 
+                            start_year_adjust=2025, 
                             end_year_adjust=2100, 
                             min_alpha=None)
 
@@ -135,7 +135,7 @@ def steel_projection(scenario: str):
     steel.project_on_total(all_regions_list_class[:-1])
     steel.smooth_out_interpolation_all(10, 2012)
     steel.adjust_alpha_and_project(all_regions_list_class[:-1], 
-                               start_year_adjust=2050, 
+                               start_year_adjust=2025, 
                                end_year_adjust=2100, 
                                min_alpha=None)
 
@@ -256,7 +256,7 @@ def aluminium_projection(scenario: str):
     aluminium.project_on_total(list(IAI_TO_IMAGE_CLASSES.keys()), start_year_projection=2012)
     aluminium.smooth_out_interpolation_all(10, 2014)
     aluminium.adjust_alpha_and_project(list(IAI_TO_IMAGE_CLASSES.keys()), 
-                               start_year_adjust=2050, 
+                               start_year_adjust=2025, 
                                end_year_adjust=2100, 
                                min_alpha=None, start_year_projection=2014)
 
