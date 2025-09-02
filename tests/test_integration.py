b8d5f9b979545c5f68609a785a17a84fed0020a0
Raoul Schram <r.d.schram@uu.nl>
2025-09-02 15:39:55 +0200

    Add to test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,7 +3,7 @@ from imagematerials.model import GenericStocks, GenericMaterials, MaterialIntens
 from imagematerials.factory import ModelFactory, Sector
 
 
-def test_vehicle_stocks(vhc_prep_data):
+def test_vehicle_stocks(vhc_prep_data, tmpdir):
     time_start = 1960
     sim_end = 1980
     complete_timeline = prism.Timeline(time_start, sim_end, 1)
@@ -13,6 +13,9 @@ def test_vehicle_stocks(vhc_prep_data):
     factory = ModelFactory(sector, complete_timeline)
     model = factory.add(GenericStocks).add(GenericMaterials).finish()
     model.simulate(simulation_timeline)
+    model.save_pkl(tmpdir / "test.pkl")
+    model2 = ModelFactory.load_pkl(tmpdir / "test.pkl")
+    assert model.stocks.equals(model2.stocks)
 
 def test_buildings_stocks(bld_prep_data):
     time_start = 1960
