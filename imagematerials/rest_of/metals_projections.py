a55fb3da1e4fc3fb1f202238f8d1eb61dc074011
Arp <a.f.arp@uu.nl>
2025-10-17 18:47:43 +0200

    updated copper trade data and fitting of groups

--- imagematerials/rest_of/metals_projections.py
+++ imagematerials/rest_of/metals_projections.py
@@ -22,30 +22,28 @@ def copper_projection(scenario: str, path_input_data, path_input_data_image):
                         path_input_data_image=path_input_data_image
                         )
 
-    class_1 = ['class_ 1'] 
+    group_1 = ["class_ 1", "class_ 24"]
+    group_2 = ["class_ 11", "class_ 23"]
+    group_3 = ["class_ 3", "class_ 12", "class_ 13", "class_ 15", 
+            "class_ 16", "class_ 18", "class_ 21", "class_ 22"]
+    group_4 = ["class_ 10", "class_ 19", "class_ 20"]
+    group_5 = ["class_ 2"]
 
-    low_steady = ['class_ 3', 'class_ 13']
-    high_gdp_low = ['class_ 2', 'class_ 11']
-    medium_steady = ['class_ 12', 'class_ 16']
-    high_steady = ['class_ 10', 'class_ 15', 'class_ 19', 'class_ 20', 'class_ 23']
 
     # trajectory not to forseen, will be fitted with global regression
-    spreaded = ['class_ 6', 'class_ 24']
-    no_data = ['class_ 4', 'class_ 5', 'class_ 7', 'class_ 8', 'class_ 9', 'class_ 14', 'class_ 17']
-    exclude = spreaded + no_data + ["class_ 18", "class_ 21", "class_ 22", "class_ 25", "class_ 26"]
-
-    # what is in rest will not be fitted because of outliers - will follow global projections       
-    rest = all_regions_list_class[:-1]
-    rest = [r for r in rest if r not in (class_1+low_steady+high_steady+medium_steady+high_gdp_low)]
+    no_data = [ "class_ 4", "class_ 5", "class_ 6", "class_ 7", 
+            "class_ 8", "class_ 9", "class_ 14", "class_ 17", 
+            "class_ 25", "class_ 26"]
+    exclude = no_data 
 
     # for these models a regression will be made
     # all reginos that are not in the high, medium, low will be fitted with the global regression
     copper_regions = {'all_regions' : all_regions_list_class[:-1],
-                    'class_ 1': class_1,
-                    'low_steady': low_steady,
-                    'high_gdp_low': high_gdp_low,
-                    'high_steady': high_steady,
-                    'medium_steady': medium_steady
+                'group_1': group_1,
+                'group_2': group_2,
+                'group_3': group_3,
+                'group_4': group_4,
+                'group_5': group_5
                 }
 
     copper.data_grouped_regions(regions_grouping = copper_regions) #list(COPPER_AVERAGE_REGIONS_TO_IMAGE.keys()
@@ -57,28 +55,26 @@ def copper_projection(scenario: str, path_input_data, path_input_data_image):
     # Fit models 
     best_rmse_models= {
         'all_regions' : 'gompertz model',
-        'class_ 1': 'gompertz model', 
-        'low_steady': 'gompertz model',
-        'high_gdp_low': 'gompertz model',
-        'high_steady': 'gompertz model',
-        'medium_steady': 'gompertz model'
+        'group_1' : 'gompertz model',
+        'group_2': 'gompertz model',
+        'group_3': 'gompertz model',
+        'group_4': 'gompertz model',
+        'group_5': 'gompertz model'
     }
-
 
     bounds = {
         'all_regions' : ([0, 0, 0], [10, 10, 10]),
-        'class_ 1': ([0, 2, 2], [10, 10, 10]),
-        'low_steady': ([0, 0, 0], [10, 10, 10]),
-        'high_gdp_low': ([0, 2, 2], [10, 10, 10]),
-        'high_steady': ([0, 0, 0], [10, 10, 10]),
-        'medium_steady': ([0, 0, 0], [10, 10, 10])
+        'group_1': ([0, 0, 0], [0.005, 10, 10]),
+        'group_2': ([0, 2, 2], [10, 10, 10]),
+        'group_3': ([0, 0, 0], [10, 10, 10]),
+        'group_4': ([0, 0, 0], [10, 10, 10]),
+        'group_5': ([0, 2, 2], [10, 10, 10])
     }
 
     copper.calculate_regressors(copper.historic_other_fraction_consumption)
     copper.fit_models(best_rmse_models=best_rmse_models, bounds=bounds)
     copper.assign_fit_to_groups_not_fitted(list_regions=all_regions_list_class[:-1], 
-                                           assign_model='low_steady', 
-                                           model_nr=6)
+                                           assign_model='group_3', model_nr=6)
 
 
     # Projections 
@@ -236,45 +232,13 @@ def aluminium_projection(scenario: str, path_input_data, path_input_data_image):
         'europe' : europe,
         'low' : low
 }
-
+    
     aluminium.data_grouped_regions(regions_grouping = aluminium_regions) 
 
     aluminium.sum_IMAGE_drivers_regions(IAI_TO_IMAGE_CLASSES)
     aluminium.match_MAT_data_to_regions_year(match_external_regions=True)
     aluminium.calculate_historic_other_fraction()
-
-
-    # Deal with negative values in other fraction BEFORE recalculation of other fraction consumption
-    # 1) Africa: for all negative values: assumption that apparent MAT is 60% of total consumption (mean until 2017)
-    # Share of IAMGE MAT data to total consumption
-    # share = (aluminium.image_mat_material_regions.loc[:2017]/aluminium.historic_consumption_data.loc[:2017]*100).mean()
-    # starting at 2017 (when negative values start):
-    # aluminium.image_mat_material_regions.loc[2017:, 'Africa'] = aluminium.historic_consumption_data.loc[2017:, 'Africa'] * share['Africa'] / 100
-
-    # 4) North America
-    # problem starts at 2015, so take share mean of IMAGE Mat data before that and apply it to 2015-2024
-    # share_na = (aluminium.image_mat_material_regions.loc[:2015]/aluminium.historic_consumption_data.loc[:2015]*100).mean()
-    # aluminium.image_mat_material_regions.loc[:, 'North America'] = aluminium.historic_consumption_data.loc[:, 'North America'] * share_na['North America'] / 100
-
-    # 6) South America: assume that share of Mat until 2010 is correct and that apparent consumption needs to be adjusted accordingly 
-    # share_sa = (aluminium.image_mat_material_regions.loc[:2010, 'South America'] / aluminium.historic_consumption_data.loc[:2010, 'South America']).mean()
-    # aluminium.historic_consumption_data.loc[:, 'South America'] = aluminium.image_mat_material_regions.loc[:, 'South America'] / share_sa
-
-    # Deal with negative values in other fraction AFTER recalculation of other fraction consumption
-    # redo calculation of historic other fraction consumption 
-    # aluminium.calculate_historic_other_fraction()
-
-    # 2) rest of Asia: 
-    # per capita consmption of total data is too low compared to other regions, so we assume that IMAGE Mat is correct
-    # --> will be fitted to global curve 
-    # 3) Estimated Unreported to IAI: assume that IMAGE Mat is correct (these are mostly IMAGE region 6 regions)
-    # Most are small island territories, overseas territories or constituent countries mainly linked to France
-    # --> will be fitted to global curve 
-
-    # 5) Gulf Cooperation Council: remove negative values to nan, no problems in more recent years
-    # & Finally check the whole dataframe for single negative values and make them NaN
     aluminium.historic_other_fraction_consumption[aluminium.historic_other_fraction_consumption < 0] = np.nan
-
 
     # Fit models
     aluminium.calculate_regressors(aluminium.historic_other_fraction_consumption)
