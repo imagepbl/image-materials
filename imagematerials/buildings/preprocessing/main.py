2cdfa2eb6685728276812d8a8db9daf063d00299
Raoul Schram <r.d.schram@uu.nl>
2026-01-05 12:04:10 +0100

    Add more docstrings to buildings preprocessing

--- imagematerials/buildings/preprocessing/main.py
+++ imagematerials/buildings/preprocessing/main.py
@@ -1,18 +1,20 @@
+"""Preprocessing of the buildings module with the main method for this."""
 import warnings
 from pathlib import Path
 
-import xarray as xr
 import numpy as np
-
-from imagematerials.constants import IMAGE_REGIONS
+import xarray as xr
 
 from imagematerials.buildings.constants import SCENARIO_SELECT
+from imagematerials.buildings.preprocessing.circular_economy_measures import (
+    apply_circular_economy_commercial_floorspace,
+)
 from imagematerials.buildings.preprocessing.floorspace import (
     compute_average_m2_capita,
     compute_housing_residential,
     compute_housing_type,
     extrapolate_floorspace,
-    get_image_floorspace
+    get_image_floorspace,
 )
 from imagematerials.buildings.preprocessing.lifetimes import compute_lifetimes
 from imagematerials.buildings.preprocessing.materials import (
@@ -20,14 +22,34 @@ from imagematerials.buildings.preprocessing.materials import (
     compute_mat_intensities_residential,
 )
 from imagematerials.buildings.preprocessing.population import compute_population
-from imagematerials.concepts import create_building_graph
-from imagematerials.concepts import create_region_graph
-
-from imagematerials.buildings.preprocessing.circular_economy_measures import apply_circular_economy_commercial_floorspace
+from imagematerials.concepts import create_building_graph, create_region_graph
+from imagematerials.constants import IMAGE_REGIONS
 
 
-def buildings_preprocessing(base_directory, climate_policy_config: dict, 
-                            circular_economy_config: dict, image_scenario: str = SCENARIO_SELECT):
+def buildings_preprocessing(base_directory: Path, climate_policy_config: dict,
+                            circular_economy_config: dict,
+                            image_scenario: str = SCENARIO_SELECT) -> xr.DataArray:
+    """Preprocess the buildings data from start to finish.
+
+    Parameters
+    ----------
+    base_directory:
+        Path to the base directory in which the input files are located.
+    climate_policy_config:
+        Climate policy configuration for which the preprocessing will be done.
+    circular_economy_config:
+        Climate economy configuration for which the preprocessing will be done.
+    image_scenario:
+        Image scenario, such as SSP2. The scenario selected should have the input
+        data available.
+
+    Returns
+    -------
+    building_preprocessing:
+        Dictionary containing the stocks, lifetimes, material_intensities, knowledge_graph,
+        and set_unit_flexible.
+
+    """
     base_directory = Path(base_directory)
     database_directory = base_directory / "buildings" / image_scenario
 
@@ -38,30 +60,38 @@ def buildings_preprocessing(base_directory, climate_policy_config: dict,
     # Get floorspace for commercial + urban/rural
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        floorspace_image_commercial_rururb, minimum_comm = get_image_floorspace(image_directory, base_directory)
-    floorspace_commercial_rururb = extrapolate_floorspace(floorspace_image_commercial_rururb, minimum_comm)
+        floorspace_image_commercial_rururb, minimum_comm = get_image_floorspace(image_directory,
+                                                                                base_directory)
+    floorspace_commercial_rururb = extrapolate_floorspace(floorspace_image_commercial_rururb,
+                                                          minimum_comm)
 
     # Rural/Urban floorspace [Time, Region, Area]
-    floorspace_rururb = floorspace_commercial_rururb.sel({"Type": ["Urban", "Rural"]}).rename({"Type": "Area"})
+    floorspace_rururb = floorspace_commercial_rururb.sel(
+        {"Type": ["Urban", "Rural"]}).rename({"Type": "Area"})
 
     # Commercial floorspace [Time, Region, Type]
     floorspace_commercial = floorspace_commercial_rururb.sel(
-        {"Type": [x.values for x in floorspace_commercial_rururb.coords["Type"] if x.values not in ["Urban", "Rural"]]})
-    
+        {"Type": [x.values for x in floorspace_commercial_rururb.coords["Type"]
+                  if x.values not in ["Urban", "Rural"]]})
+
     if "base" or "narrow" in circular_economy_config.keys():
         # Implement circular economy for commercial floorspace
-        # This is only done for the base and narrow scenarios, as the other scenarios do not have a circular economy component
-        floorspace_commercial = apply_circular_economy_commercial_floorspace(floorspace_commercial, circular_economy_config)
-        
+        # This is only done for the base and narrow scenarios, as the other scenarios do not have a
+        # circular economy component
+        floorspace_commercial = apply_circular_economy_commercial_floorspace(floorspace_commercial,
+                                                                             circular_economy_config)
+
     # Calculate population ("Total", "Rural", "Urban")
     population = compute_population(image_directory, base_directory)
-    
+
     average_m2_capita = compute_average_m2_capita(base_directory)
 
     housing_type = compute_housing_type(database_directory)
 
-    floorspace_residential = compute_housing_residential(population, average_m2_capita, housing_type, floorspace_rururb, circular_economy_config)
-    
+    floorspace_residential = compute_housing_residential(population, average_m2_capita,
+                                                         housing_type, floorspace_rururb,
+                                                         circular_economy_config)
+
     # Commercial floorspace also needs to be multiplied by population & drop Area dimension
     floorspace_commercial_total = floorspace_commercial * population.sel({"Area": "Total"})
     floorspace_commercial_total = floorspace_commercial_total.drop_vars("Area")
@@ -71,27 +101,37 @@ def buildings_preprocessing(base_directory, climate_policy_config: dict,
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        lifetimes = compute_lifetimes(base_directory, floorspace_commercial.coords["Type"].values, circular_economy_config)
+        lifetimes = compute_lifetimes(base_directory, floorspace_commercial.coords["Type"].values,
+                                      circular_economy_config)
 
-    mat_intensities_comm = compute_mat_intensities_commercial(database_directory, circular_economy_config)
-    mat_intensities_res = compute_mat_intensities_residential(database_directory, circular_economy_config)
+    mat_intensities_comm = compute_mat_intensities_commercial(database_directory,
+                                                              circular_economy_config)
+    mat_intensities_res = compute_mat_intensities_residential(database_directory,
+                                                              circular_economy_config)
     mat_intensities = xr.concat((mat_intensities_res, mat_intensities_comm), dim="Type")
     knowledge_graph_buildings = create_building_graph()
     mat_intensities = knowledge_graph_buildings.rebroadcast_xarray(
                             mat_intensities, floorspace.coords["Type"].values)
-    
+
     #TODO remove this quick fix
     region_coords = np.sort(floorspace.coords["Region"].values.astype(int)).astype(str)
-    floorspace = knowledge_graph_buildings.rebroadcast_xarray(floorspace, region_coords, dim ="Region")
+    floorspace = knowledge_graph_buildings.rebroadcast_xarray(floorspace, region_coords,
+                                                              dim ="Region")
 
-    
+
     # convert region names to the standard IMAGE regions
     knowledge_graph_region = create_region_graph()
 
-    floorspace = knowledge_graph_region.rebroadcast_xarray(floorspace, output_coords=IMAGE_REGIONS, dim="Region")
+    floorspace = knowledge_graph_region.rebroadcast_xarray(floorspace, output_coords=IMAGE_REGIONS,
+                                                           dim="Region")
     for key, value in lifetimes.items():
-        lifetimes[key] = knowledge_graph_region.rebroadcast_xarray(value, output_coords=IMAGE_REGIONS, dim="Region")
-    mat_intensities = knowledge_graph_region.rebroadcast_xarray(mat_intensities, output_coords=IMAGE_REGIONS, dim="Region")
+        lifetimes[key] = knowledge_graph_region.rebroadcast_xarray(value,
+                                                                   output_coords=IMAGE_REGIONS,
+                                                                   dim="Region")
+    mat_intensities = knowledge_graph_region.rebroadcast_xarray(mat_intensities,
+                                                                output_coords=IMAGE_REGIONS,
+                                                                dim="Region")
 
     return {"stocks": floorspace, "lifetimes": lifetimes, "material_intensities": mat_intensities,
-            "knowledge_graph": knowledge_graph_buildings, "set_unit_flexible": str(floorspace.pint.units)}
+            "knowledge_graph": knowledge_graph_buildings,
+            "set_unit_flexible": str(floorspace.pint.units)}
--- imagematerials/buildings/preprocessing/materials.py
+++ imagematerials/buildings/preprocessing/materials.py
@@ -1,22 +1,31 @@
+"""Preprocessing module for the material intensities."""
 import pandas as pd
-
-import numpy as np
-import xarray as xr
 import prism
-
-
-from imagematerials.concepts import create_region_graph
+from pathlib import Path
 
 from imagematerials.buildings.constants import END_YEAR, HIST_YEAR
-from imagematerials.buildings.preprocessing.circular_economy_measures import (circular_economy_measures_material_intensities_residential,
-                                                                              circular_economy_measures_material_intensities_commercial)
+from imagematerials.buildings.preprocessing.circular_economy_measures import (
+    circular_economy_measures_material_intensities_commercial,
+    circular_economy_measures_material_intensities_residential,
+)
 from imagematerials.util import dataset_to_array
 
 idx = pd.IndexSlice
 
-def compute_mat_intensities_residential(database_dir, circular_economy_config: dict | None=None, **_,):
-    building_materials = pd.read_csv(database_dir / 'Building_materials_rasmi.csv', index_col = [0,1,2])   # Building_materials; unit: kg/m2; meaning: the average material use per square meter (by building type, by region & by area)
+def compute_mat_intensities_residential(database_dir: Path,
+                                        circular_economy_config: dict | None = None,
+                                        **_,):
+    """Compute the material intensities for residential buildings.
     
+    Parameters
+    ----------
+    database_dir:
+        
+    """
+    # Building_materials; unit: kg/m2; meaning: the average material use per square meter
+    # (by building type, by region & by area)
+    building_materials = pd.read_csv(database_dir / 'Building_materials_rasmi.csv', index_col = [0,1,2]) 
+
     if 'resource_efficient' in circular_economy_config.keys():
         building_materials = pd.read_csv(database_dir / 'Building_materials_rasmi_resource_efficient.csv', index_col = [0,1,2])   # Building_materials; unit: kg/m2; meaning: the average material use per square meter (by building type, by region & by area)
         print("Applied using resource_efficient building materials intensities for residential buildings.")
@@ -34,10 +43,10 @@ def compute_mat_intensities_residential(database_dir, circular_economy_config: d
     xr_mat_res_intensities = dataset_to_array(building_materials_dynamic.to_xarray(), ["Cohort", "Region", "Type"], ["material"])
     xr_mat_res_intensities.coords["Type"] = ["Detached", "Semi-detached", "Appartment", "High-rise"]
     xr_mat_res_intensities.coords["Region"] = [str(x) for x in xr_mat_res_intensities.coords["Region"].values]
- 
+
     # applying material intensity changes for residential buildings
     if 'narrow' in circular_economy_config.keys():
-        xr_mat_res_intensities = circular_economy_measures_material_intensities_residential(xr_mat_res_intensities, 
+        xr_mat_res_intensities = circular_economy_measures_material_intensities_residential(xr_mat_res_intensities,
                                                                                             circular_economy_config)
 
     xr_mat_res_intensities = prism.Q_(xr_mat_res_intensities, "kg/m^2") # assign unit
@@ -46,7 +55,7 @@ def compute_mat_intensities_residential(database_dir, circular_economy_config: d
 
 
 def compute_mat_intensities_commercial(database_dir, circular_economy_config: dict | None=None, **_,):
-    materials_commercial = pd.read_csv(database_dir / 'materials_commercial_rasmi.csv', index_col = [0,1]) # 7 building materials in 4 commercial building types; unit: kg/m2; meaning: the average material use per square meter (by commercial building type) 
+    materials_commercial = pd.read_csv(database_dir / 'materials_commercial_rasmi.csv', index_col = [0,1]) # 7 building materials in 4 commercial building types; unit: kg/m2; meaning: the average material use per square meter (by commercial building type)
 
     #First: interpolate the dynamic material intensity data
     materials_commercial_dynamic = pd.DataFrame(index=pd.MultiIndex.from_product([list(range(HIST_YEAR, END_YEAR + 1)), list(materials_commercial.index.levels[1])]), columns=materials_commercial.columns)
