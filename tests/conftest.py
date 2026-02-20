0443533dccbe5776ce3e01638a22ad5fbcedba39
Frederike Arp <152859146+frede-arp@users.noreply.github.com>
2026-02-20 17:31:20 +0100

    515 create tests from different file path (#517)
    * changed test scen to more up to date scen, which is in the same scen folder as other scens
    
    * changed test data files and removed old image files
    
    * change directory of conftest files

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,12 @@ import pytest
 
 from imagematerials.preprocessing import get_preprocessing_data
 
+path_test_scenario = Path("data", "raw", "image", "SSP2_M_CP")
 
 @pytest.fixture(scope="session")
 def vhc_sector():
-    return get_preprocessing_data("vehicles", Path("data", "raw"))
+    return get_preprocessing_data("vehicles", Path("data", "raw"), 
+                                  climate_policy_scenario_dir = path_test_scenario)
 
 
 @pytest.fixture(scope="session")
@@ -16,7 +18,8 @@ def vhc_prep_data(vhc_sector):
 
 @pytest.fixture(scope="session")
 def bld_sector():
-    return get_preprocessing_data("buildings", Path("data", "raw"))
+    return get_preprocessing_data("buildings", Path("data", "raw"), 
+                                  climate_policy_scenario_dir = path_test_scenario)
 
 @pytest.fixture(scope="session")
 def bld_prep_data(bld_sector):
@@ -25,13 +28,9 @@ def bld_prep_data(bld_sector):
 @pytest.fixture(scope="session")
 def elc_sector():
     path_base = Path("data", "raw")
-    VARIANT = "VLHO"
-    SCEN = "SSP2"
-    scen_folder = SCEN + "_" + VARIANT
-    climate_policy_scenario_dir = Path(path_base, "image", scen_folder)
+    climate_policy_scenario_dir = Path(path_base, "image", "SSP2_M_CP")
     return get_preprocessing_data("electricity", base_dir=path_base,
-                                  climate_policy_scenario_dir=climate_policy_scenario_dir,
-                                  standard_scenario=scen_folder)
+                                  climate_policy_scenario_dir=climate_policy_scenario_dir)
 
 @pytest.fixture(scope="session")
 def elc_prep_data(elc_sector):
