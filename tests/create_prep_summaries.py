7cc923c460f9e4a3c44132673f7aa381f6937a66
Arp <a.f.arp@uu.nl>
2026-02-27 13:43:27 +0100

    add energy unrelated data

--- tests/create_prep_summaries.py
+++ tests/create_prep_summaries.py
@@ -9,7 +9,7 @@ from imagematerials.util import (
 )
 
 base_directory = Path("data", "raw")
-climate_policy_scenario_dir = base_directory.joinpath("image", "SSP2_baseline")
+climate_policy_scenario_dir = base_directory.joinpath("image", "SSP2_BL")
 
 if __name__ == "__main__":
     # Vehicles summary
