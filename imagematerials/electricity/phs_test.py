224e8d8f2d797ce0f8998419738c60802f767c9b
Judith T. <judith.tettenborn@mailbox.org>
2026-04-09 17:25:39 +0200

    added IHA PHS data; added script for handling those data

---
+++ imagematerials/electricity/phs_test.py
@@ -0,0 +1,22 @@
+#%%
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import pint
+import xarray as xr
+
+
+from imagematerials.constants import IMAGE_REGIONS
+
+path_current = Path().resolve()
+path_base = path_current.parent.parent # base path of the project -> image-materials
+path_data = Path(path_base, "data", "raw")
+
+df_phs =  pd.read_csv(Path(path_data,"electricity","IHA_PSH_Capacity_data.csv"))
+
+ds = df_phs.to_xarray()
+
+ds_test = ds.copy()
+ds_test = ds_test.set_coords(["Country", "Commisioning Year"])
+ds_test["Capacity"].groupby(["Country", "Commisioning Year"]).sum()
+
