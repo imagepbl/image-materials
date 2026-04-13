a14efddbbf68f11860479efb02014bad611e373e
MarianneZZotin <m.zanonzotin@uu.nl>
2026-04-13 14:59:43 +0200

    create structure for ff preprocessing

---
+++ imagematerials/fossil_fuels/preprocessing/materials.py
@@ -0,0 +1,5 @@
+# import relevant libraries
+# import constants and other functions within image materials
+# read in materials files for each stage
+# convert to xarrays
+# return material_intensity_extraction,material_intensity_processing,... 
