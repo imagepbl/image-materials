f81aad039023abff16ca301b59cdd236eb880ec3
Roel Brouwer <5979700+RoelBrouwer@users.noreply.github.com>
2026-07-23 14:26:57 +0200

    Implement test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,11 +1,13 @@
 import prism
 import pytest
+import numpy as np
 from pathlib import Path
 from imagematerials.model import GenericStocks, GenericMaterials, MaterialIntensities
 from imagematerials.factory import ModelFactory, Sector
-from imagematerials.vehicles.preprocessing.util import get_passengerkms, get_tonkms
+from imagematerials.vehicles.preprocessing.util import simulate_timer_data
 from imagematerials.util import read_climate_policy_config
-from imagematerials.vehicles.timermodel import TIMERMaterials
+# from imagematerials.vehicles.timermodel import TIMERMaterials
+from imagematerials.vehicles.stocks_prism import VehicleStocks
 
 
 @pytest.fixture(scope="session")
@@ -14,19 +16,54 @@ def pickle_path(tmp_path_factory):
     return temp_path
 
 
-def test_vehicle_stocks(vhc_prep_data, pickle_path):
-    time_start = 1960
-    sim_end = 1980
-    complete_timeline = prism.Timeline(time_start, sim_end, 1)
-    simulation_timeline = prism.Timeline(1970, sim_end, 1)
+@pytest.fixture(scope="module")
+def time_start():
+    return 1960  # prism.Q_(1960, 'year')
 
+
+@pytest.fixture(scope="module")
+def simulation_start():
+    return 1971  # prism.Q_(1971, 'year')
+
+
+@pytest.fixture(scope="module")
+def simulation_end():
+    return 1980  # prism.Q_(1980, 'year')
+
+
+@pytest.fixture(scope="module")
+def complete_timeline(time_start, simulation_end):
+    return prism.Timeline(
+        start=time_start,
+        end=simulation_end,
+        stepsize=1  # prism.Q_(1, 'year')
+    )
+
+
+@pytest.fixture(scope="module")
+def simulation_timeline(simulation_start, simulation_end):
+    return prism.Timeline(
+        start=simulation_start,
+        end=simulation_end,
+        stepsize=1  # prism.Q_(1, 'year')
+    )
+
+
+@pytest.fixture(scope="module")
+def vhc_model(vhc_prep_data, complete_timeline, simulation_timeline):
     sector = Sector("vhc", vhc_prep_data)
     factory = ModelFactory(sector, complete_timeline)
     model = factory.add(GenericStocks).add(GenericMaterials).finish()
     model.simulate(simulation_timeline)
+    return model
+
+
+def test_vehicle_stocks(vhc_model, pickle_path):
+    model = vhc_model
     model.save_pkl(pickle_path / "test.pkl")
     model2 = ModelFactory.load_pkl(pickle_path / "test.pkl")
     assert model.stocks.equals(model2.stocks)
+
 
 def test_buildings_stocks(bld_prep_data):
     time_start = 1960
@@ -54,51 +91,49 @@ def test_combined_stocks(bld_prep_data, vhc_prep_data):
     assert len(model.stocks) == 2
 
 
-def test_timer_interface(pickle_path):
+def test_timer_interface(vhc_model, complete_timeline, simulation_timeline):
     # Run simulation both in standalone and simulated TIMER-coupled mode.
     # Ensure results are the same.
-    sim_end = prism.Q_(1980, 'year')
-    complete_timeline = prism.Timeline(
-        start=prism.Q_(1960, 'year'),
-        end=sim_end,
-        stepsize=prism.Q_(1, 'year')
-    )
-    simulation_timeline = prism.Timeline(
-        start=prism.Q_(1970, 'year'),
-        end=sim_end,
-        stepsize=prism.Q_(1, 'year')
-    )
 
     # Reuse model output from earlier test
     # TODO: make this work in a more clean way
-    vehicles_model = ModelFactory.load_pkl(pickle_path / "test.pkl")
+    vhc_model = vhc_model
 
     # Get the raw data that otherwise would come from TIMER
     path_test_scenario = Path("data", "raw", "image", "SSP2_baseline")
-    climate_policy_config = read_climate_policy_config(path_test_scenario)
+    timer_data = simulate_timer_data(path_test_scenario)
+
     # Retrieve TIMER transport activity inputs from preprocessing helpers.
-    passenger_kms = get_passengerkms(path_test_scenario, climate_policy_config)
-    tonkms = get_tonkms(path_test_scenario, climate_policy_config)
+    passenger_kms = timer_data["passenger_kms"]
+    tonne_kms = timer_data["tonne_kms"]
+    load_car = timer_data["load_car"]
+
     def timer_input(t):
         return {
-            "passenger_kms": passenger_kms.loc[t],
-            "tonkms": tonkms.loc[t]
+            "passenger_kms": passenger_kms.sel(Time=t),
+            "tonne_kms": tonne_kms.sel(Time=t),
+            "load_car": load_car.sel(Time=t)
         }
 
-    timer_vehicle_model = TIMERMaterials(complete_timeline) # TODO: fix signature
+    # For now only testing the VehicleStocks
+    timer_vehicle_model = VehicleStocks(complete_timeline, path_test_scenario)
     # Simulate TIMER's input using the timer_input function
     timer_vehicle_model.simulate(simulation_timeline, inputs=timer_input)
     
     # Compare outputs of both model runs
     # TODO: decide what to compare
-    vehicles_model.vehicles.get("inflow_materials")
-    timer_vehicle_model.get("inflow_materials")
+    compare_total_vehicles_stocks(vhc_model.stocks, timer_vehicle_model.total_vehicles, simulation_timeline)  # timer_vehicle_model.stocks
+    # assert False
 
-def compare_total_vehicles_stocks_to_original(self, total, time):
-    for vehicle in total.coords["Type"].values:
-        # skip these for now as they are still need to be split up in suptypes in the new calculation
-        # compare to 1e-6 to account for small discrepancies in data handling
-        if vehicle not in ["Light Commercial Vehicles", "Cars", "Medium Freight Trucks", "Heavy Freight Trucks", "Regular Buses", "Midi Buses"]:
-            compare_new = self.stocks_original.sel(Type = vehicle).loc[int(prism.M_(time))].sum()
-            compare_old = total.sel(Type=vehicle).sum()
-            assert abs(compare_new - compare_old) < 1e-6, f"Discrepancy in {vehicle} calculation for time {time}: original total {self.stocks_original.sel(Type = vehicle).loc[int(prism.M_(time))].sum().values}, new total {total.sel(Type=vehicle).sum().values}"
+def compare_total_vehicles_stocks(standalone_stocks, timer_stocks, timeline):
+    # Get simulation years to compare
+    for time in range(timeline.start, timeline.end, timeline.stepsize):
+        if time != timeline.end:
+            continue
+        for vehicle in timer_stocks.coords["Type"].values:
+            # skip these for now as they are still need to be split up in suptypes in the new calculation
+            # compare to 1e-6 to account for small discrepancies in data handling
+            if vehicle not in ["Light Commercial Vehicles", "Cars", "Medium Freight Trucks", "Heavy Freight Trucks", "Regular Buses", "Midi Buses"]:
+                compare_old = standalone_stocks.sel(Type = vehicle).loc[int(prism.M_(time))].sum()
+                compare_new = timer_stocks.sel(Type=vehicle).sum()
+                assert np.isclose(compare_old, compare_new), f"Discrepancy in {vehicle} calculation for time {time}: original total {standalone_stocks.sel(Type = vehicle).loc[int(prism.M_(time))].sum().values}, new total {timer_stocks.sel(Type=vehicle).sum().values}"
