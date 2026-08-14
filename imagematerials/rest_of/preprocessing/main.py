67a5be2e00eade202423a35606ded8cfa2943db4
Arp <a.f.arp@uu.nl>
2026-08-14 13:14:48 +0200

    add SSP2 VVLO Lifetech to list where res of takes resource efficiency scenario

--- imagematerials/rest_of/preprocessing/main.py
+++ imagematerials/rest_of/preprocessing/main.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from imagematerials.constants import IMAGE_REGIONS
 from imagematerials.concepts import create_region_graph
 
+from imagematerials.factory import ModelFactory
 from imagematerials.read_mym import read_mym_df
 from imagematerials.buildings.preprocessing.population import compute_population
 
@@ -17,39 +18,6 @@ from imagematerials.rest_of.preprocessing.regressions_all_materials import (fit_
                                                                             historic_other_fraction_consumption_to_xr)
 
 from imagematerials.rest_of.util import sum_inflows_for_all_sectors, save_sum_as_csv
-
-
-def sum_and_save(model_run, list_sum_sectors: list, save_path: Path, mfa_data = 'inflow_materials'):
-    """
-    Sums inflows across specified sectors and saves the results for steel, aluminium, and copper
-    Parameters
-    ----------
-    model_run :
-        ImageMaterials model instance providing sector attributes (buildings, vehicles, generation, grid, ...)
-        each exposing `.get(get_mfa_data).to_array()` returning an xarray.DataArray.
-    list_sum_sectors : list
-        List of sector names to include in the sum (e.g. ["buildings", "vehicles", "generation", "grid"]).
-    mfa_data : str, optional
-        Key/name of the MFA data to extract from each sector (default is 'inflow_materials').
-        Could also be stock, or outflow_materials.
-    Returns
-    -------
-    xarray.DataArray
-        Elementwise sum of the sector inflows for the specified sectors.
-    """
-
-    total_inflow_combined = sum_inflows_for_all_sectors(model_run, mfa_data, list_sum_sectors)
-
-    # save materials
-    save_sum_as_csv(total_inflow_combined, "steel", save_path)
-    save_sum_as_csv(total_inflow_combined, "aluminium", save_path)
-    save_sum_as_csv(total_inflow_combined, "copper", save_path)
-    save_sum_as_csv(total_inflow_combined, "cement", save_path)
-    save_sum_as_csv(total_inflow_combined, "sand_gravel_crushed_rock", save_path)
-
-    print("Materials saved successfully.")
-    return total_inflow_combined
-
 
 def read_gompertz_values(base_directory, scenario: str):
     """
@@ -80,7 +48,7 @@ def read_gompertz_values(base_directory, scenario: str):
 
     if scenario in ["SSP2_resource_efficiency", "SSP2_narrow_activity","SSP2_narrow", "SSP2_narrow_slow_close", 
                     "SSP2_narrow_act_26_tax","SSP2_narrow_26_tax", "SSP2_narrow_slow_close_26_tax",
-                    "SSP2_narrow_slow_close_19_tax"]:
+                    "SSP2_narrow_slow_close_19_tax", "SSP2_VLLO_LifeTech"]:
         print('Using Gompertz coefficients for resource efficiency measures')
         name = "coefs_gompertz_eff.nc"
     else: 
